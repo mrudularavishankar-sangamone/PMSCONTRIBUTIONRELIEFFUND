ac2fdf1cb3bef3f97fa51b5236dd82ad06ae0e3e
mrudularavishankar-sangamone <mrudularavishankar-sangamone@gmail.com>
2025-12-17 17:58:39 +0530

    Adding Display/View + Validation for state and pincode

---
+++ relieffund-app/src/pages/ContributionViews.jsx
@@ -0,0 +1,8 @@
+export default function ContributionView() {
+  return (
+    <div className="contributionViewCSS">
+      <h2> Contribution List </h2>
+      <p> This is where the contributions will be displayed. </p>
+    </div>
+  );
+}
--- relieffund-app/src/pages/DonationForm.jsx
+++ relieffund-app/src/pages/DonationForm.jsx
@@ -1,3 +1,5 @@
+import ContributionView from './ContributionViews.jsx';
+
 export default function DonationForm() {
  
   function handleSubmit(event) {
@@ -8,15 +10,17 @@ export default function DonationForm() {
     const name = document.getElementById("name").value;
     const eID = document.getElementById("eID").value;
     const orgName = document.getElementById("orgName").value;
-    const donateAmount = document.getElementById("donateAmount").value;
+    const donatedAmount = document.getElementById("donatedAmount").value;
+    const state = document.getElementById("state").value;
+    const pinCode = document.getElementById("pinCode").value;
     
     //Validating form inputs
-    if(name === "" || eID === "" || orgName === "" || donateAmount === "") {
+    if(name === "" || eID === "" || orgName === "" || donatedAmount === "" || state === "" || pinCode === "") {
       alert("Please fill in all the required fields.");
       return;
     }
     else {
-      alert(`Thank you '${name}' for your generous donation of '₹${donateAmount}' to PM's Relief Fund! Your Employee ID: '${eID}' has been recorded.`);
+      alert(`Thank you '${name}' for your generous donation of '₹${donatedAmount}' to PM's Relief Fund! Your Employee ID: '${eID}' has been recorded.`);
     }
 
     //Resetting the form after submission
@@ -35,33 +39,44 @@ export default function DonationForm() {
   }
   
   return (
-    <div className = "donationFormCSS">
+    <>
+      <div className = "donationFormCSS">
 
-      <h2> Donation Form </h2>
-      
-      <form onSubmit = {handleSubmit} >
-        <label> Enter your name:
-          <input type="text" id = "name" pattern = "^[a-zA-Z]+$" onInvalid = {(e) => handleInvalid(e, 'Name must contain only alphabets.')} onInput = {handleInput} required/>
-        </label>
-        
-        <label> Enter your employee ID:
-          {/* Employee ID must be alphanumeric and at least 5 characters long */}
-          <input type="text" id = "eID" pattern = "(?=.*\d)(?=.*[a-zA-Z]).{5,}" onInvalid = {(e) => handleInvalid(e, 'EmployeeID must be Alphanumeric and atleast 5 characters long.')} onInput = {handleInput} required/>
-        </label>
-        
-        <label> Enter your organization name:
-          <input type="text" id = "orgName" required/>
-        </label>
-        
-        <label> Enter the amount you wish to donate: 
-          <input type="number" min = "100" max = "500" id = "donateAmount" required/>
-        </label>
+        <h2> Donation Form </h2>
 
-        <button className = "submitButton" type = "submit"> Submit Donation </button>
-        <button className = "resetButton" type = "reset"> Reset Form </button>
+        <form onSubmit = {handleSubmit} >
+          <label> Enter your name:
+            <input type="text" id = "name" pattern = "^[a-zA-Z]+$" onInvalid = {(e) => handleInvalid(e, 'Name must contain only alphabets.')} onInput = {handleInput} required/>
+          </label>
 
-      </form>
-    
-    </div>
+          <label> Enter your employee ID:
+            {/* Employee ID must be alphanumeric and at least 5 characters long */}
+            <input type="text" id = "eID" pattern = "(?=.*\d)(?=.*[a-zA-Z]).{5,}" onInvalid = {(e) => handleInvalid(e, 'EmployeeID must be Alphanumeric and atleast 5 characters long.')} onInput = {handleInput} required/>
+          </label>
+
+          <label> Enter your organization name:
+            <input type="text" id = "orgName" required/>
+          </label>
+
+          <label> Enter your State of Employment:
+            <input type="text" id = "state" pattern = "^[a-zA-Z]+$" onInvalid = {(e) => handleInvalid(e, 'State Name must contain only alphabets.')} onInput = {handleInput} required/>
+          </label>
+
+          <label> Enter the Pincode of your Org:
+            <input type="text" id = "pinCode" pattern="\\d{6}" minLength={6} maxLength={6} onInvalid = {(e) => handleInvalid(e, 'Enter exactly six digits (0-9) only')} onInput = {handleInput}  required/>
+          </label>
+
+          <label> Enter the amount you wish to donate: 
+            <input type="number" min = "100" max = "500" id = "donatedAmount" required/>
+          </label>
+
+          <button className = "submitButton" type = "submit"> Submit Donation </button>
+          <button className = "resetButton" type = "reset"> Reset Form </button>
+
+        </form>
+      </div>
+
+      <ContributionView />
+    </>
   );
 }
